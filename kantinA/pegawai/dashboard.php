be8b8cb3b24ac3d685c4f12c93c66cea2229e4b4
Ariel Saputra <m15759246@gmail.com>
2023-12-08 13:52:36 +0700

    perubahan baruu

--- kantinA/pegawai/dashboard.php
+++ kantinA/pegawai/dashboard.php
@@ -16,9 +16,9 @@ $b = mysqli_fetch_array($key);
 <div class="main-panel">
     <div class="content">
         <div class="my-3" style="margin: 0 2rem;">
-			<h1 class="fw-bold">Dashboard Pegawai</h1>
-			<hr>
-		</div>
+            <h1 class="fw-bold">Dashboard Pegawai</h1>
+            <hr>
+        </div>
         <div class="page-inner">
             <div class="row">
                 <div class="col-sm-6 col-md-6">
@@ -27,18 +27,15 @@ $b = mysqli_fetch_array($key);
                             <div class="row align-items-center">
                                 <div class="col-icon">
                                     <div class="icon-big text-center icon-primary bubble-shadow-small">
-                                    <i class="fa fa-money" aria-hidden="true"></i>
+                                        <i class="fa fa-money" aria-hidden="true"></i>
                                     </div>
                                 </div>
                                 <div class="col col-stats ml-3 ml-sm-0">
                                     <div class="numbers">
                                         <p class="card-category">Data Transaksi</p>
                                         <h4 class="card-title"><?php echo $row['transaksi'] ?></h4>
-<<<<<<< HEAD
-                                        <a href="?view=datatransaksi" class="btn btn-primary"><i class="fa fa-folder-open-o" aria-hidden="true"></i>Kelola</a>
-=======
-                                        <a href="?view=datatransaksi" class="btn btn-primary"><i class="fa fa-folder-open-o" aria-hidden="true"></i> Kelola</a>
->>>>>>> 775a34432d55a786c70c33ed89ae2d49ba191e11
+                                        <a href="?view=datatransaksi" class="btn btn-primary"><i
+                                                class="fa fa-folder-open-o" aria-hidden="true"></i> Kelola</a>
                                     </div>
                                 </div>
                             </div>
@@ -59,11 +56,8 @@ $b = mysqli_fetch_array($key);
                                     <div class="numbers">
                                         <p class="card-category">Data Pengajuan Barang</p>
                                         <h4 class="card-title"><?php echo $q['pengajuan'] ?></h4>
-<<<<<<< HEAD
-                                        <a href="?view=datapengajuan" class="btn btn-primary"><i class="fa fa-folder-open-o" aria-hidden="true"></i>Kelola</a>
-=======
-                                        <a href="?view=datapengajuan" class="btn btn-primary"><i class="fa fa-folder-open-o" aria-hidden="true"></i> Kelola</a>
->>>>>>> 775a34432d55a786c70c33ed89ae2d49ba191e11
+                                        <a href="?view=datapengajuan" class="btn btn-primary"><i
+                                                class="fa fa-folder-open-o" aria-hidden="true"></i> Kelola</a>
                                     </div>
                                 </div>
                             </div>
@@ -77,22 +71,15 @@ $b = mysqli_fetch_array($key);
                             <div class="row align-items-center">
                                 <div class="col-icon">
                                     <div class="icon-big text-center icon-primary bubble-shadow-small">
-<<<<<<< HEAD
-                                        <i class="fas fa-check-circle"></i>
-=======
                                         <i class="fa fa-suitcase"></i>
->>>>>>> 775a34432d55a786c70c33ed89ae2d49ba191e11
                                     </div>
                                 </div>
                                 <div class="col col-stats ml-3 ml-sm-0">
                                     <div class="numbers">
                                         <p class="card-category">Data Jenis Barang</p>
                                         <h4 class="card-title"><?php echo $b['barang'] ?></h4>
-<<<<<<< HEAD
-                                        <a href="?view=databarang" class="btn btn-primary"><i class="fa fa-folder-open-o" aria-hidden="true"></i>Kelola</a>
-=======
-                                        <a href="?view=datajenisbarang" class="btn btn-primary"><i class="fa fa-folder-open-o" aria-hidden="true"></i> Kelola</a>
->>>>>>> 775a34432d55a786c70c33ed89ae2d49ba191e11
+                                        <a href="?view=datajenisbarang" class="btn btn-primary"><i
+                                                class="fa fa-folder-open-o" aria-hidden="true"></i> Kelola</a>
                                     </div>
                                 </div>
                             </div>
@@ -106,22 +93,15 @@ $b = mysqli_fetch_array($key);
                             <div class="row align-items-center">
                                 <div class="col-icon">
                                     <div class="icon-big text-center icon-primary bubble-shadow-small">
-<<<<<<< HEAD
-                                        <i class="fas fa-check-circle"></i>
-=======
                                         <i class="fas fa-briefcase"></i>
->>>>>>> 775a34432d55a786c70c33ed89ae2d49ba191e11
                                     </div>
                                 </div>
                                 <div class="col col-stats ml-3 ml-sm-0">
                                     <div class="numbers">
                                         <p class="card-category">Data Barang</p>
                                         <h4 class="card-title"><?php echo $b['barang'] ?></h4>
-<<<<<<< HEAD
-                                        <a href="?view=databarang" class="btn btn-primary"><i class="fa fa-folder-open-o" aria-hidden="true"></i>Kelola</a>
-=======
-                                        <a href="?view=databarang" class="btn btn-primary"><i class="fa fa-folder-open-o" aria-hidden="true"></i> Kelola</a>
->>>>>>> 775a34432d55a786c70c33ed89ae2d49ba191e11
+                                        <a href="?view=databarang" class="btn btn-primary"><i
+                                                class="fa fa-folder-open-o" aria-hidden="true"></i> Kelola</a>
                                     </div>
                                 </div>
                             </div>
