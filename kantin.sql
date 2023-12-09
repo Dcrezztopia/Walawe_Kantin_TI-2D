fade75ec6bc3754e515c8e9f759e812eb8a76c34
Ariel Saputra <m15759246@gmail.com>
2023-12-09 19:59:44 +0700

    Upload file

--- kantin.sql
+++ kantin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 05 Des 2023 pada 04.59
+-- Waktu pembuatan: 08 Des 2023 pada 08.20
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -30,67 +30,12 @@ SET time_zone = "+00:00";
 CREATE TABLE `barang` (
   `idBarang` int(11) NOT NULL,
   `namaBarang` varchar(225) NOT NULL,
-  `jenisBarang` varchar(50) NOT NULL,
+  `jenisBarang` varchar(225) NOT NULL,
   `stok` varchar(225) NOT NULL,
   `harga` decimal(10,0) NOT NULL,
   `sku` varchar(255) NOT NULL,
-  `idSupplier` int(11) NOT NULL
+  `namaSupplier` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `barang`
---
-
-INSERT INTO `barang` (`idBarang`, `namaBarang`, `jenisBarang`, `stok`, `harga`, `sku`, `idSupplier`) VALUES
-(2, 'Cinnamon Swirl Delight', 'Roti', '50', '3', 'CSD002', 2),
-(4, 'Sesame Seed Sourdough', 'Roti', '80', '4', 'SSS004', 3),
-(5, 'Chocolate Chip Brioche', 'Roti', '60', '5', 'CCB005', 4),
-(6, 'Whole Wheat Wonder Loaf', 'Roti', '90', '2', 'WWL006', 5),
-(7, 'Rustic Rye Boule', 'Roti', '120', '4', 'RRB007', 6),
-(8, 'Sweet Potato Sourdough', 'Roti', '40', '5', 'SPS008', 7),
-(9, 'Honey Oat Harvest Loaf', 'Roti', '70', '4', 'HOHL009', 8),
-(10, 'Sunflower Seed Bagel', 'Roti', '110', '3', 'SSB010', 9),
-(11, 'Pumpkin Spice Twist', 'Roti', '55', '3', 'PST011', 10),
-(12, 'Blueberry Bliss Scone', 'Kue', '85', '2', 'BBS012', 11),
-(13, 'Almond Joy Danish', 'Kue', '65', '4', 'AJD013', 12),
-(14, 'Chocolate Hazelnut Croissant', 'Kue', '95', '5', 'CHC014', 13),
-(15, 'Raspberry Lemon Cupcake', 'Kue', '75', '3', 'RLC015', 14),
-(16, 'Vanilla Bean Macaron', 'Kue', '100', '4', 'VBM016', 15),
-(17, 'Red Velvet Whoopie Pie', 'Kue', '45', '4', 'RVWP017', 16),
-(18, 'Pistachio Rose Eclair', 'Kue', '80', '6', 'PRE018', 17),
-(19, 'Lemon Blueberry Muffin', 'Kue', '60', '3', 'LBM019', 18),
-(20, 'Salted Caramel Brownie', 'Kue', '70', '4', 'SCB020', 19),
-(21, 'Peanut Butter Cup Cookie', 'Kue', '105', '2', 'PBCC021', 20),
-(22, 'Coconut Cream Pie Slice', 'Kue', '50', '3', 'CCPS022', 21),
-(23, 'Gingerbread House Kit', 'Kue', '75', '5', 'GHK023', 22),
-(24, 'Peach Cobbler Bar', 'Kue', '95', '3', 'PCB024', 23),
-(25, 'Chocolate Mint Whoopie Pie', 'Kue', '55', '3', 'CMWP025', 24),
-(26, 'Maple Pecan Danish', 'Kue', '85', '4', 'MPD026', 25),
-(27, 'Strawberry Shortcake Cupcake', 'Kue', '65', '3', 'SSCC027', 26),
-(28, 'Cherry Almond Tart', 'Kue', '90', '4', 'CAT028', 27),
-(29, 'Mango Passionfruit Macaron', 'Kue', '110', '4', 'MPM029', 28),
-(30, 'Hazelnut Chocolate Scone', 'Kue', '40', '5', 'HCS030', 29),
-(31, 'Key Lime Pie Slice', 'Kue', '70', '3', 'KLPS031', 30),
-(32, 'Banana Nut Muffin', 'Kue', '95', '4', 'BNM032', 31),
-(33, 'Raspberry Chocolate Brownie', 'Kue', '50', '3', 'RCB033', 32),
-(34, 'Blueberry Cheesecake Bar', 'Kue', '75', '4', 'BCB034', 33),
-(35, 'Pumpkin Spice Cupcake', 'Kue', '85', '4', 'PSC035', 34),
-(36, 'Oreo Cookie Cheesecake Slice', 'Kue', '60', '3', 'OCCS036', 35),
-(37, 'Lemon Poppy Seed Muffin', 'Kue', '80', '3', 'LPSM037', 36),
-(38, 'Salted Caramel Chocolate Tart', 'Kue', '55', '5', 'SCCT038', 37),
-(39, 'Coconut Mango Cupcake', 'Kue', '65', '3', 'CMC039', 38),
-(40, 'Almond Joy Cupcake', 'Kue', '90', '4', 'AJC040', 39),
-(41, 'Chocolate Raspberry Truffle Tart', 'Kue', '40', '6', 'CRTT041', 40),
-(42, 'Cranberry Orange Scone', 'Kue', '105', '3', 'COS042', 41),
-(43, 'Chocolate Peanut Butter Brownie', 'Kue', '50', '3', 'CPBB043', 42),
-(44, 'Red Velvet Cheesecake Bar', 'Kue', '75', '4', 'RVCB044', 43),
-(45, 'Apple Cinnamon Muffin', 'Kue', '95', '3', 'ACM045', 44),
-(46, 'Pecan Pie Slice', 'Kue', '55', '3', 'PPS046', 45),
-(47, 'Mocha Hazelnut Cupcake', 'Kue', '80', '5', 'MHC047', 46),
-(48, 'Lavender Honey Tart', 'Kue', '75', '4', 'LHT048', 47),
-(49, 'Apricot Almond Scone', 'Kue', '65', '3', 'AAS049', 48),
-(50, 'Dark Chocolate Truffle Cupcake', 'Kue', '110', '4', 'DCTC050', 49),
-(51, 'ariel', 'dennis', '24', '17', '', 7);
 
 -- --------------------------------------------------------
 
@@ -107,69 +52,22 @@ CREATE TABLE `detailtransaksi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `supplier`
+-- Struktur dari tabel `jenisbarang`
 --
 
-CREATE TABLE `supplier` (
-  `idsupplier` int(11) NOT NULL,
-  `namaSupplier` varchar(225) NOT NULL
+CREATE TABLE `jenisbarang` (
+  `jenisBarang` varchar(225) NOT NULL,
+  `deskripsi` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `supplier`
+-- Dumping data untuk tabel `jenisbarang`
 --
 
-INSERT INTO `supplier` (`idsupplier`, `namaSupplier`) VALUES
-(1, 'Golden Wheat Suppliers'),
-(2, 'Flour Fusion Inc.'),
-(3, 'Dough Delights Co.'),
-(4, 'Yeast Paradise'),
-(5, 'Bakery Bliss Suppliers'),
-(6, 'Sweet Grain Traders'),
-(7, 'Rising Sun Distributors'),
-(8, 'Dough Master Supply'),
-(9, 'Flaky Flour Enterprises'),
-(10, 'Grain Galore Corp.'),
-(11, 'Tasty Treats Suppliers'),
-(12, 'Rolling Pins Unlimited'),
-(13, 'Wholesome Wheat Merchants'),
-(14, 'Sugar and Spice Trading'),
-(15, 'Crunchy Crust Distributors'),
-(16, 'Butter and Flour Connection'),
-(17, 'Delightful Dough Inc.'),
-(18, 'Sweets and Sourdough Co.'),
-(19, 'Flour Fantasy Traders'),
-(20, 'Golden Crust Enterprises'),
-(21, 'Rye and Shine Distributors'),
-(22, 'Crispy Crust Suppliers'),
-(23, 'Sweet Wheat Emporium'),
-(24, 'Hearty Harvest Trading'),
-(25, 'Artisanal Breadcraft Co.'),
-(26, 'Perfect Pastries Ltd.'),
-(27, 'Flavorful Flour Co.'),
-(28, 'Savory Slices Traders'),
-(29, 'Whisk and Wheat Merchants'),
-(30, 'Bread Boutique Inc.'),
-(31, 'Dough Dynasty'),
-(32, 'Sourdough Symphony Suppliers'),
-(33, 'Rolling in Rye Co.'),
-(34, 'Sweet Symphony Traders'),
-(35, 'Flour Fusion Enterprises'),
-(36, 'Bake Bliss Distributors'),
-(37, 'Golden Grain Guild'),
-(38, 'Hearty Harvest Trading'),
-(39, 'Crispy Crust Creations'),
-(40, 'Flaky Fantasy Co.'),
-(41, 'Yeast Yield Traders'),
-(42, 'Butter and Baguette Enterprises'),
-(43, 'Rising Rolls Co.'),
-(44, 'Artisanal Appetites Trading'),
-(45, 'Flourish and Flavor Co.'),
-(46, 'Bread Basket Emporium'),
-(47, 'Wholesome Wheat Trading'),
-(48, 'Dough Divinity Suppliers'),
-(49, 'Flavorful Flour Traders'),
-(50, 'Rolling in Riches Co.');
+INSERT INTO `jenisbarang` (`jenisBarang`, `deskripsi`) VALUES
+('Makanan Berat', 'Jenis barang makanan berat'),
+('Makanan Ringan', 'Jenis barang makanan ringan'),
+('Minuman', 'Jenis barang minuman');
 
 -- --------------------------------------------------------
 
@@ -181,7 +79,8 @@ CREATE TABLE `transaksi` (
   `kodeTransaksi` int(11) NOT NULL,
   `jumlahitem` int(225) NOT NULL,
   `totalPembayaran` decimal(50,0) NOT NULL,
-  `nip` int(11) NOT NULL
+  `nip` int(11) NOT NULL,
+  `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -223,15 +122,6 @@ CREATE TABLE `waitingroom` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `waitingroom`
---
-
-INSERT INTO `waitingroom` (`id_waiting`, `namabarang`, `jenisbarang`, `sku`, `namasupplier`, `harga`, `status`) VALUES
-(1, 'Dennis Parulian Panjaitan', 'Manusia', '32345', 'anjaymabar', '3', 'menunggu'),
-(3, 'Ariel', 'Manusia juga kok', '32134', 'adadeh', '34', 'disetujui'),
-(4, 'Pascalis', 'anjaynmabar', '324567', 'walawe', '3', 'ditolak');
-
---
 -- Indexes for dumped tables
 --
 
@@ -240,7 +130,8 @@ INSERT INTO `waitingroom` (`id_waiting`, `namabarang`, `jenisbarang`, `sku`, `na
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`idBarang`),
-  ADD KEY `fk_idSupplier` (`idSupplier`);
+  ADD KEY `fk_idSupplier` (`namaSupplier`),
+  ADD KEY `fk_jenis` (`jenisBarang`);
 
 --
 -- Indeks untuk tabel `detailtransaksi`
@@ -251,10 +142,10 @@ ALTER TABLE `detailtransaksi`
   ADD KEY `fk_idBarang` (`idBarang`);
 
 --
--- Indeks untuk tabel `supplier`
+-- Indeks untuk tabel `jenisbarang`
 --
-ALTER TABLE `supplier`
-  ADD PRIMARY KEY (`idsupplier`);
+ALTER TABLE `jenisbarang`
+  ADD PRIMARY KEY (`jenisBarang`);
 
 --
 -- Indeks untuk tabel `transaksi`
@@ -283,19 +174,13 @@ ALTER TABLE `waitingroom`
 -- AUTO_INCREMENT untuk tabel `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `idBarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `idBarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT untuk tabel `detailtransaksi`
 --
 ALTER TABLE `detailtransaksi`
   MODIFY `kodedetail` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `supplier`
---
-ALTER TABLE `supplier`
-  MODIFY `idsupplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaksi`
@@ -323,7 +208,7 @@ ALTER TABLE `waitingroom`
 -- Ketidakleluasaan untuk tabel `barang`
 --
 ALTER TABLE `barang`
-  ADD CONSTRAINT `fk_idSupplier` FOREIGN KEY (`idSupplier`) REFERENCES `supplier` (`idsupplier`);
+  ADD CONSTRAINT `fk_jenis` FOREIGN KEY (`jenisBarang`) REFERENCES `jenisbarang` (`jenisBarang`);
 
 --
 -- Ketidakleluasaan untuk tabel `detailtransaksi`
--- kantinA/pegawai/dataPegawai/transaksi/createtransaksi.php
+++ kantinA/pegawai/dataPegawai/transaksi/createtransaksi.php
@@ -1,160 +1,149 @@
-
-
 <div class="main-panel">
-			<div class="content">
-				<div class="page-inner">
-					<div class="page-header">
-						<h4 class="page-title">Transaksi</h4>
-						<ul class="breadcrumbs">
-							<li class="nav-home">
-								<a href="#">
-									<i class="flaticon-home"></i>
+	<div class="content">
+		<div class="page-inner">
+			<div class="page-header">
+				<h4 class="page-title">Transaksi</h4>
+				<ul class="breadcrumbs">
+					<li class="nav-home">
+						<a href="#">
+							<i class="flaticon-home"></i>
+						</a>
+					</li>
+					<li class="separator">
+						<i class="flaticon-right-arrow"></i>
+					</li>
+					<li class="nav-item">
+						<a href="#">Transaksi</a>
+					</li>
+					<li class="separator">
+						<i class="flaticon-right-arrow"></i>
+					</li>
+					<li class="nav-item">
+						<a href="#">Tambah Transaksi</a>
+					</li>
+				</ul>
+			</div>
+
+			<!-- TAMBAH TRANSAKSI -->
+			<div class="row">
+				<div class="col-md-12">
+					<div class="card">
+						<div class="card-header">
+							<div class="card-title">Tambah Transaksi
+								<a href="?view=datatransaksi">
+									<button type="button" class="close" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
 								</a>
-							</li>
-							<li class="separator">
-								<i class="flaticon-right-arrow"></i>
-							</li>
-							<li class="nav-item">
-								<a href="#">Transaksi</a>
-							</li>
-							<li class="separator">
-								<i class="flaticon-right-arrow"></i>
-							</li>
-							<li class="nav-item">
-								<a href="#">Tambah Transaksi</a>
-							</li>
-						</ul>
-					</div>
-
-					<!-- TAMBAH TRANSAKSI -->
-					<div class="row">
-						<div class="col-md-12">
-							<div class="card">
-								<div class="card-header">
-									<div class="card-title">Tambah Transaksi
-										<a href="?view=datatransaksi">
-										<button type="button" class="close" aria-label="Close">
-											<span aria-hidden="true">&times;</span>
-										</button>
-										</a>
-									</div>
-								</div>
-								<form method="POST" action="" enctype="multipart/form-data">
-								<div class="card-body">
-									<div class="form-group" id="skuFormGroup">
-										<label class="fw-bold">SKU</label>
-										<input type="text" name="sku" id="skuInput" class="form-control" placeholder="RT001-001-STRW" required="" >
-										<span>Sisa Barang: XX</span>
-									</div>
-
-									<div class="form-group" id="jumlahFormGroup">
-										<label class="fw-bold">Jumlah</label>
-										<input type="number" name="harga" id="jumlahInput" class="form-control" placeholder="Masukkan Jumlah.." required="">
-									</div>
-
-									<div class="form-group" id="idTransaksiFormGroup">
-										<label class="fw-bold">ID Transaksi</label>
-										<input type="text" id="idTransaksi" class="form-control" value="12345" readonly>
-									</div>
-
-									<div class="form-group" id="totalNominal">
-										<label class="fw-bold">Total</label>
-									</div>
-
-									<div class="form-group" id="angkaNominal">
-										<span id="total" style="color: red; font-size: 40px">1000</span>
-									</div>
-
-									<div class="card-action">
-										<button type="submit" name="tambah" class="btn btn-primary">Tambah</button>
-										<button type="submit" name="bayar" class="btn btn-success">Bayar</button>
-									</div>
-									</div>
-
-								</form>
-
-								<div class="card-body">
-								<div class="table-responsive">
-										<table id="add-row" class="display table table-striped table-hover" >
-											<thead>
-												
-												<tr>
-													<th>No</th>
-													<th>Foto</th>
-													<th>SKU</th>
-													<th>Nama Barang</th>													
-													<th>Harga</th>
-													<th>Jumlah</th>
-													<th>Action</th>
-												</tr>
-											</thead>
-											
-											<tbody>
-												<td></td>
-												<td></td>
-												<td></td>
-												<td></td>
-												<td></td>
-												<td></td>
-												<td>	
-													<a href="" data-toggle="modal" title="Hapus" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
-												</td>
-											</tbody>
-										</table>
-									</div>
-								</div>
-							</div>
 							</div>
 						</div>
-</div>
+						<form method="POST" action="" enctype="multipart/form-data">
+							<div class="card-body">
+								<div class="form-group" id="skuFormGroup">
+									<label class="fw-bold">SKU</label>
+									<input type="text" name="sku" id="skuInput" class="form-control" placeholder="RT001-001-STRW" required="">
+									<span>Sisa Barang: XX</span>
+								</div>
+
+								<div class="form-group" id="jumlahFormGroup">
+									<label class="fw-bold">Jumlah</label>
+									<input type="number" name="harga" id="jumlahInput" class="form-control" placeholder="Masukkan Jumlah.." required="">
+								</div>
+
+								<div class="form-group" id="idTransaksiFormGroup">
+									<label class="fw-bold">ID Transaksi</label>
+									<input type="text" id="idTransaksi" class="form-control" value="12345" readonly>
+								</div>
+
+								<div class="form-group" id="totalNominal">
+									<label class="fw-bold">Total</label>
+								</div>
+
+								<div class="form-group" id="angkaNominal">
+									<span id="total" style="color: red; font-size: 40px">1000</span>
+								</div>
+
+								<div class="card-action">
+									<button type="submit" name="tambah" class="btn btn-primary">Tambah</button>
+									<button type="submit" name="bayar" class="btn btn-success">Bayar</button>
+								</div>
+							</div>
+
+						</form>
+
+						<div class="card-body">
+							<div class="table-responsive">
+								<table id="add-row" class="display table table-striped table-hover">
+									<thead>
+										<tr>
+											<th>No</th>
+											<th>Id Transaksi</th>
+											<th>Tanggal</th>
+											<th>Jumlah</th>
+											<th>Total Pembayaran</th>
+											<th>NIP Pegawai</th>
+										</tr>
+									</thead>
+									<tbody>
+										<?php
+										$no = 1;
+										$query = mysqli_query($conn, 'SELECT * FROM transaksi');
+										while ($transaksi = mysqli_fetch_array($query)) {
+										?>
+											<tr>
+												<td><?php echo $no++ ?></td>
+												<td><?php echo $transaksi['kodeTransaksi'] ?></td>
+												<td><?php echo $transaksi['tanggal'] ?></td>
+												<td><?php echo $transaksi['jumlahitem'] ?></td>
+												<td><?php echo $transaksi['totalPembayaran'] ?></td>
+												<td><?php echo $transaksi['nip'] ?></td>
+											</tr>
+										<?php } ?>
+									</tbody>
+								</table>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
 
 		<script type="text/javascript">
-			<?php 
-				echo $stok;
-				echo $deskripsi;
-				echo $nama_barang;
+			<?php
+			echo $stok;
+			echo $deskripsi;
+			echo $nama_barang;
 			?>
-			function changeValue(id){
+
+			function changeValue(id) {
 				document.getElementById('stok').value = stok[id].stok;
-				document.getElementById('deskripsi').value = deskripsi[id].deskripsi;
 				document.getElementById('nama_barang').value = nama_barang[id].nama_barang;
 			};
 		</script>
 
 
 		<?php
-		    if (isset($_POST['simpan'])) {
-			
-		        $id_barang = $_POST['id_barang'];
-		        $qty = $_POST['qty'];
-		        $tgl_mulai = $_POST['tgl_mulai'];
-		        $tgl_selesai = $_POST['tgl_selesai'];
-		        $lokasi_barang = $_POST['lokasi_barang'];
-		        $id_user = $_POST['id_user'];
-		        $status = $_POST['status'];
-		        
-		        $email_user = $_POST['email_user'];
-		        $email_admin = $_POST['email_admin'];
-		        $password_user = $_POST['password_user'];
-		        $nama_barang = $_POST['nama_barang'];
+		if (isset($_POST['simpan'])) {
+			// Ambil data dari formulir
+			$id_barang = $_POST['id_barang'];
+			$id_user = $_POST['id_user'];
+			$qty = $_POST['qty'];
+			$tgl_mulai = $_POST['tgl_mulai'];
+			$tgl_selesai = $_POST['tgl_selesai'];
+			$lokasi_barang = $_POST['lokasi_barang'];
+			$status = $_POST['status'];
+			$email_user = $_POST['email_user'];
+			$email_admin = $_POST['email_admin'];
+			$password_user = $_POST['password_user'];
+			$nama_barang = $_POST['nama_barang'];
 
-		        $selSto =mysqli_query($conn, "SELECT * FROM barang WHERE id='$id_barang'");
-			    $sto    =mysqli_fetch_array($selSto);
-			    $stok    =$sto['stok'];
-			    //menghitung sisa stok
-			    $sisa    =$stok-$qty;
+			// Lakukan operasi penyimpanan data sesuai kebutuhan
+			// ...
 
-			    if($stok < $qty){
-			    	echo "<script>alert ('Stok Kurang Dari Jumlah Pinjam') </script>";
-
-			    }else{
-				    mysqli_query($conn,"INSERT into pinjambarang values ('','$id_barang', '$id_user','$qty','$tgl_mulai','$tgl_selesai','$lokasi_barang','$status')");
-		            mysqli_query($conn, "UPDATE barang SET stok='$sisa' WHERE id='$id_barang'");
-
-		            echo "<script>alert ('Data Berhasil Disimpan') </script>";
-                	echo "<script>window.location.replace('?view=datapinjambarang');</script>";
-				}
-
-		    }
-		    ?>
+			// Tampilkan pesan atau redirect ke halaman yang sesuai
+			echo "<script>alert('Data Berhasil Disimpan')</script>";
+			echo "<script>window.location.replace('?view=datapinjambarang');</script>";
+		}
+		?>
