4049ed2edffae1ea308cc8a311fc57216e0590a5
Fyodor-DOs <fathir123t@gmail.com>
2023-12-18 08:59:01 +0700

    Upload file

--- kantinA/admin/index.php
+++ kantinA/admin/index.php
@@ -2,24 +2,30 @@
 include '../koneksi.php';
 session_start();
 ?>
-<?php 
-$p = mysqli_query($conn,'SELECT count(*) as waitingroom from waitingroom where status = "menunggu"');
+<?php
+$p = mysqli_query($conn, 'SELECT count(*) as waitingroom from waitingroom where status = "menunggu"');
 $q = mysqli_fetch_array($p);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>Sistem Informasi Kantin</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="../assets/img/logo.png" type="image/x-icon"/>
-	
+	<link rel="icon" href="../assets/img/logo.png" type="image/x-icon" />
+
 	<!-- Fonts and icons -->
 	<script src="../assets/js/plugin/webfont/webfont.min.js"></script>
 	<script>
 		WebFont.load({
-			google: {"families":["Open+Sans:300,400,600,700"]},
-			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands"], urls: ['../assets/css/fonts.css']},
+			google: {
+				"families": ["Open+Sans:300,400,600,700"]
+			},
+			custom: {
+				"families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands"],
+				urls: ['../assets/css/fonts.css']
+			},
 			active: function() {
 				sessionStorage.fonts = true;
 			}
@@ -28,22 +34,23 @@ $q = mysqli_fetch_array($p);
 
 	<style>
 		.logo img.navbar-brand {
-    		width: 120px;
-    		height: auto;
+			width: 120px;
+			height: auto;
 		}
 
 		.nav-item.active a {
-            background-color: #007bff;
-            color: #fff;
-        }
+			background-color: #007bff;
+			color: #fff;
+		}
 	</style>
-	
+
 	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="../assets/css/azzara.css">
 	<link rel="stylesheet" href="../assets/css/demo.css">
 </head>
+
 <body>
 	<div class="wrapper">
 		<!--
@@ -52,10 +59,10 @@ $q = mysqli_fetch_array($p);
 		<div class="main-header" data-background-color="orange">
 			<!-- Logo Header -->
 			<div class="logo-header">
-				
+
 				<a href="#" class="logo">
-                    <img src="../assets/img/logo2.png" alt="navbar brand" class="navbar-brand">
-                </a>
+					<img src="../assets/img/logo2.png" alt="navbar brand" class="navbar-brand">
+				</a>
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">
 						<i class="fa fa-bars"></i>
@@ -73,7 +80,7 @@ $q = mysqli_fetch_array($p);
 			<!-- Navbar Header -->
 			<nav class="navbar navbar-header navbar-expand-lg">
 				<div class="container-fluid">
-					
+
 				</div>
 			</nav>
 			<!-- End Navbar -->
@@ -81,19 +88,19 @@ $q = mysqli_fetch_array($p);
 
 		<!-- Sidebar -->
 		<div class="sidebar">
-        <div class="sidebar-wrapper scrollbar-inner">
-            <div class="sidebar-content">
-                <ul class="nav list-group">
-                    	<li class="nav-item">
+			<div class="sidebar-wrapper scrollbar-inner">
+				<div class="sidebar-content">
+					<ul class="nav list-group">
+						<li class="nav-item">
 							<a href="?view=dashboard" class="nav-link">
 								<i class="fas fa-home"></i>
-								<p>Dashboard</p>
+								<p>Dashboard Admin</p>
 							</a>
 						</li>
 						<li class="nav-section">
 							<span class="sidebar-mini-icon">
-                                <i class="fa fa-ellipsis-h"></i>
-                            </span>
+								<i class="fa fa-ellipsis-h"></i>
+							</span>
 							<h4 class="text-section">Components</h4>
 						</li>
 
@@ -127,12 +134,12 @@ $q = mysqli_fetch_array($p);
 						</li>
 
 						<li class="nav-section">
-                            <span class="sidebar-mini-icon">
-                                <i class="fa fa-cog" aria-hidden="true"></i>
-                            </span>
+							<span class="sidebar-mini-icon">
+								<i class="fa fa-cog" aria-hidden="true"></i>
+							</span>
 							<h4 class="text-section">Setting</h4>
 						</li>
-					
+
 						<li class="nav-item">
 							<a href="?view=profil" class="nav-link">
 								<i class="fa fa-user" aria-hidden="true"></i>
@@ -140,40 +147,40 @@ $q = mysqli_fetch_array($p);
 							</a>
 						</li>
 
-                    	<li class="nav-item">
-                            <a href="#" class="nav-link" data-toggle="modal" data-target="#logoutConfirmationModal">
-                                <i class="fas fa-lock"></i>
-                                <p>Logout</p>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+						<li class="nav-item">
+							<a href="#" class="nav-link" data-toggle="modal" data-target="#logoutConfirmationModal">
+								<i class="fas fa-lock"></i>
+								<p>Logout</p>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 
-        <div class="modal fade" id="logoutConfirmationModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header no-bd">
+		<div class="modal fade" id="logoutConfirmationModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header no-bd">
 						<h5 class="modal-title">
-							<span class="fw-mediumbold">Logout</span> 
+							<span class="fw-mediumbold">Logout</span>
 							<span class="fw-light">Admin</span>
 						</h5>
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-								<span aria-hidden="true">&times;</span>
-							</button>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
 					</div>
-                    <div class="modal-body">
+					<div class="modal-body">
 						<input type="hidden" name="id" value="<?php echo $row['id'] ?>">
-					    <h4>Apakah anda ingin <b>Logout</b>?</h4>
+						<h4>Apakah anda ingin <b>Logout</b>?</h4>
 					</div>
-                    <div class="modal-footer no-bd">
-                        <button class="btn btn-primary" type="button" data-dismiss="modal"><i class="fa fa-times" aria-hidden="true"></i>&nbsp;Cancel</button>
-                        <a class="btn btn-danger" href="../logout.php"><i class="fas fa-lock"></i>&nbsp;Logout</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+					<div class="modal-footer no-bd">
+						<button class="btn btn-primary" type="button" data-dismiss="modal"><i class="fa fa-times" aria-hidden="true"></i>&nbsp;Cancel</button>
+						<a class="btn btn-danger" href="../logout.php"><i class="fas fa-lock"></i>&nbsp;Logout</a>
+					</div>
+				</div>
+			</div>
+		</div>
 
 		<!-- JS NOTIF BELL SAMPING TULISAN PENGAJUAN -->
 		<!-- <script>
@@ -213,40 +220,40 @@ $q = mysqli_fetch_array($p);
 		?> -->
 
 		<?php
-                    // Dashboard
-                    if(@$_GET['view']=='')
-                        include 'dashboard.php';
-                    elseif($_GET['view']=='dashboard')
-                        include 'dashboard.php';
+		// Dashboard
+		if (@$_GET['view'] == '')
+			include 'dashboard.php';
+		elseif ($_GET['view'] == 'dashboard')
+			include 'dashboard.php';
 
-                    // Data Barang
-                    elseif($_GET['view']=='databarang')
-                        include 'master/barang/databarang.php';
-                    
-                    // Data Pengajuan
-                    elseif($_GET['view']=='datapengajuan')
-                        include 'master/pengajuan/datapengajuan.php';
+		// Data Barang
+		elseif ($_GET['view'] == 'databarang')
+			include 'master/barang/databarang.php';
 
-                    // Data Jenis Barang
-                    elseif($_GET['view']=='datajenisbarang')
-                        include 'master/jenisbarang/datajenisbarang.php';
-                    
-					// Data Laporan
-                    elseif($_GET['view']=='datalaporan')
-                        include 'master/laporan/datalaporan.php';
-                   
-					// Profil
-                    elseif($_GET['view']=='profil')
-                        include 'master/profil/profil.php';
-                    elseif($_GET['view']=='editprofil')
-                        include 'master/profil/editprofil.php';
-						
-                 ?>
+		// Data Pengajuan
+		elseif ($_GET['view'] == 'datapengajuan')
+			include 'master/pengajuan/datapengajuan.php';
+
+		// Data Jenis Barang
+		elseif ($_GET['view'] == 'datajenisbarang')
+			include 'master/jenisbarang/datajenisbarang.php';
+
+		// Data Laporan
+		elseif ($_GET['view'] == 'datalaporan')
+			include 'master/laporan/datalaporan.php';
+
+		// Profil
+		elseif ($_GET['view'] == 'profil')
+			include 'master/profil/profil.php';
+		elseif ($_GET['view'] == 'editprofil')
+			include 'master/profil/editprofil.php';
+
+		?>
 
 		<!-- Add Bootstrap JS and jQuery -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 		<script src="../assets/js/core/jquery.3.2.1.min.js"></script>
 		<script src="../assets/js/core/popper.min.js"></script>
 		<script src="../assets/js/core/bootstrap.min.js"></script>
@@ -263,25 +270,25 @@ $q = mysqli_fetch_array($p);
 		<script src="../assets/js/ready.min.js"></script>
 		<!-- Azzara DEMO methods, don't include it in your project! -->
 		<script src="../assets/js/setting-demo.js"></script>
-		<script >
+		<script>
 			$(document).ready(function() {
-				$('#add-row').DataTable({
-				});
+				$('#add-row').DataTable({});
 			});
 		</script>
 		<!-- jQuery script to add "active" class to clicked menu item -->
 		<script>
-        $(document).ready(function () {
-            var currentUrl = window.location.href;
+			$(document).ready(function() {
+				var currentUrl = window.location.href;
 
-            $(".nav-item a").each(function () {
-                if (currentUrl.indexOf($(this).attr("href")) > -1) {
-                    $(this).closest('.nav-item').addClass("active");
-                }
-            });
-        });
-    	</script>
+				$(".nav-item a").each(function() {
+					if (currentUrl.indexOf($(this).attr("href")) > -1) {
+						$(this).closest('.nav-item').addClass("active");
+					}
+				});
+			});
+		</script>
 
 	</div>
 </body>
+
 </html>
--- kantinA/admin/master/laporan/datalaporan.php
+++ kantinA/admin/master/laporan/datalaporan.php
@@ -26,218 +26,235 @@
 									<h4 class="card-title">Filter Laporan</h4>
 								</div>
 							</div>
+
 							<div class="row card-body">
-								
+
 								<div class="col-md-4">
 									<div class="form-group mb-3">
 										<label for="inputMulaiTanggal" class="font-weight-bold">Mulai Tanggal :</label>
 										<input type="date" id="inputMulaiTanggal" class="form-control" name="mulai_tanggal" value="<?php echo $mulai_tanggal ?>" required>
 									</div>
 								</div>
-								
+
 								<div class="col-md-4">
 									<div class="form-group mb-3">
-									<label for="inputSampaiTanggal" class="font-weight-bold">Sampai Tanggal :</label>
-									<input type="date" id="inputSampaiTanggal" class="form-control" name="sampai_tanggal" value="<?php echo $sampai_tanggal ?>" required>
+										<label for="inputSampaiTanggal" class="font-weight-bold">Sampai Tanggal :</label>
+										<input type="date" id="inputSampaiTanggal" class="form-control" name="sampai_tanggal" value="<?php echo $sampai_tanggal ?>" required>
+									</div>
 								</div>
-							</div>
-							<div class="col-md-4 d-flex align-items-center mt-2">
-								<button type="submit" name="tampil" class="btn btn-primary"><i class="fa fa-filter" aria-hidden="true"></i>&nbsp; Tampilkan</button>
-							</div>
-							
-							
-						</div>
-					</div>
-					
-					<div class="card">
-						<div class="card-header">
-							<div class="d-flex align-items-center">
-								<h4 class="card-title">Laporan Penghasilan</h4>
-							</div>
-						</div>
-						<div class="row card-body">
-							
-							<div class="col-md-4">
-							<?php
-															$no = 1;
-															$query = mysqli_query($conn,'SELECT nilai_omset FROM omset where id = 1');
-															while ($omset = mysqli_fetch_array($query)) {
-														?>
-								<div class="form-group mb-3">
-									<label>Omzet</label>
-									<input readonly value="<?php echo $omset['nilai_omset'] ?>" type="number" name="omset" class="form-control" >
+								<div class="col-md-4 d-flex align-items-center mt-2">
+									<button type="submit" name="tampil" class="btn btn-primary"><i class="fa fa-filter" aria-hidden="true"></i>&nbsp; Tampilkan</button>
 								</div>
-							</div>
-							<?php } ?>
-						</form>
-							
-							<div class="col-md-4 d-flex align-items-center mt-2">
-								<a href="cetaklaporan.php" target="_blank">
-									<button type="button" name="cetak" class="btn btn-success"><i class="fa fa-print" aria-hidden="true"></i>&nbsp; Cetak</button>
-								</a>
-							</div>
-							<div class="table-responsive">
-							<table id="add-row" class="display table table-striped table-hover">
-											<thead>
-												<tr>
-													<th>No</th>
-													<th>ID Transaksi</th>
-													<th>Tanggal</th>
-													<th>Jumlah Item</th>
-													<th>Total Pembayaran</th>                                    
-													<th>NIP Pegawai</th>
-													<th>Action</th>         
-												</tr>
-											</thead>
 
-											<tbody>
-												<?php   
-														$query_omset_date = mysqli_query($conn, "SELECT tanggalMulai, tanggalSelesai FROM omset WHERE id = 1");
-														$omset_date = mysqli_fetch_assoc($query_omset_date);
-														$tanggal_mulai_omset = $omset_date['tanggalMulai'];
-														$tanggal_selesai_omset = $omset_date['tanggalSelesai'];
-														$no = 1;
-														$query = mysqli_query($conn, "SELECT * FROM transaksi WHERE tanggal BETWEEN '$tanggal_mulai_omset' AND '$tanggal_selesai_omset'");
-															while ($transaksi = mysqli_fetch_array($query)) {
-														?>
-												
-												<tr>
-													<td><?php echo $no++ ?></td>
-													<td><?php echo $transaksi['kodeTransaksi'] ?></td>
-													<td><?php echo $transaksi['tanggal'] ?></td>
-													<td><?php echo $transaksi['jumlahitem'] ?></td>
-													<td><?php echo $transaksi['totalPembayaran'] ?></td>
-													<td><?php echo $transaksi['nip'] ?></td>
-													<td>
-													<a href="#modalDetailBarang<?php echo $transaksi['kodeTransaksi'] ?>"  data-toggle="modal" title="Detail" class="btn btn-xs btn-primary"><i class="fa fa-eye"></i></a>
-													</td>
-												</tr>
-											
-												<?php } ?>
-											</tbody>
-										</table>
+
 							</div>
 						</div>
-					</div>
-				</div>
+
+						<div class="card">
+							<div class="card-header">
+								<div class="d-flex align-items-center">
+									<h4 class="card-title">Laporan Penghasilan</h4>
+								</div>
+							</div>
+							<div class="row card-body">
+								<div class="col-md-5 mb-4 ml-3">
+									<table class="table table-bordered border-light no-border">
+										<tbody>
+											<tr>
+												<td class="font-weight-bold">Mulai Tanggal</td>
+												<td class="font-weight-bold">:</td>
+												<td><?php echo $tanggal['tanggalMulai'] ?></td>
+											</tr>
+											<tr>
+												<td class="font-weight-bold">Sampai Tanggal</td>
+												<td class="font-weight-bold">:</td>
+												<td><?php echo $tanggal['tanggalSelesai'] ?></td>
+											</tr>
+										</tbody>
+									</table>
+
+									<div class="row">
+										<?php
+										$no = 1;
+										$query = mysqli_query($conn, 'SELECT nilai_omset FROM omset where id = 1');
+										while ($omset = mysqli_fetch_array($query)) {
+										?>
+											<div class="form-group col-md-7">
+												<label>Omzet</label>
+												<input readonly value="<?php echo $omset['nilai_omset'] ?>" type="number" name="omset" class="form-control">
+											</div>
+
+											<div class="d-flex align-items-center form-group mt-4">
+												<a href="cetaklaporan.php" target="_blank">
+													<button type="button" name="cetak" class="btn btn-success w-100"><i class="fa fa-print" aria-hidden="true"></i>&nbsp; Cetak</button>
+												</a>
+											</div>
+									</div>
+								<?php } ?>
+
+								</div>
+			</form>
+
+
+			<div class="table-responsive">
+				<table id="add-row" class="display table table-striped table-hover">
+					<thead>
+						<tr>
+							<th>No</th>
+							<th>ID Transaksi</th>
+							<th>Tanggal</th>
+							<th>Jumlah Item</th>
+							<th>Total Pembayaran</th>
+							<th>NIP Pegawai</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<?php
+						$query_omset_date = mysqli_query($conn, "SELECT tanggalMulai, tanggalSelesai FROM omset WHERE id = 1");
+						$omset_date = mysqli_fetch_assoc($query_omset_date);
+						$tanggal_mulai_omset = $omset_date['tanggalMulai'];
+						$tanggal_selesai_omset = $omset_date['tanggalSelesai'];
+						$no = 1;
+						$query = mysqli_query($conn, "SELECT * FROM transaksi WHERE tanggal BETWEEN '$tanggal_mulai_omset' AND '$tanggal_selesai_omset'");
+						while ($transaksi = mysqli_fetch_array($query)) {
+						?>
+
+							<tr>
+								<td><?php echo $no++ ?></td>
+								<td><?php echo $transaksi['kodeTransaksi'] ?></td>
+								<td><?php echo $transaksi['tanggal'] ?></td>
+								<td><?php echo $transaksi['jumlahitem'] ?></td>
+								<td><?php echo $transaksi['totalPembayaran'] ?></td>
+								<td><?php echo $transaksi['nip'] ?></td>
+								<td>
+									<a href="#modalDetailBarang<?php echo $transaksi['kodeTransaksi'] ?>" data-toggle="modal" title="Detail" class="btn btn-xs btn-primary"><i class="fa fa-eye"></i></a>
+								</td>
+							</tr>
+
+						<?php } ?>
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
 </div>
+</div>
+</div>
+</div>
+</div>
 
-<?php 
-										$q = mysqli_query($conn, 'SELECT * FROM transaksi');
-									
-										while($k = mysqli_fetch_array($q)) {
-									?>
+<?php
+$q = mysqli_query($conn, 'SELECT * FROM transaksi');
 
-									<div class="modal fade" id="modalDetailBarang<?php echo $k['kodeTransaksi'] ?>" tabindex="-1" role="dialog" aria-hidden="true">
-										<div class="modal-dialog" role="document">
-											<div class="modal-content">
-												<div class="modal-header no-bd">
-													<h5 class="modal-title">
-														<span class="fw-mediumbold">
-														Detail</span> 
-														<span class="fw-light">
-															Barang
-														</span>
-													</h5>
-													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-														<span aria-hidden="true">&times;</span>
-													</button>
-												</div>
-												<form method="POST" enctype="multipart/form-data" action="">
-												<div class="modal-body">
-													<input type="hidden" name="id" value="<?php echo $k['kodeTransaksi'] ?>">
-													<div class="form-group">
-														<label>Kode Transaksi</label>
-														<input readonly value="<?php echo $k['kodeTransaksi'] ?>" type="number" name="kodeTransaksi" class="form-control" >
-													</div>
-													<div class="form-group">
-														<label>Tanggal</label>
-														<input readonly value="<?php echo $k['tanggal'] ?>" type="date" name="tanggal" class="form-control" >
-													</div>
-													<div class="form-group">
-														<label>Jumlah Item</label>
-														<input readonly value="<?php echo $k['jumlahitem'] ?>" type="number" name="jumlahitem" class="form-control">
-													</div>
-													<div class="form-group">
-														<label>Total Pembayaran</label>
-														<input readonly value="<?php echo $k['totalPembayaran'] ?>" type="number" name="totalpembayaran" class="form-control">
-													</div>
-													<div class="form-group">
-														<label>Pegawai yang menangani</label>
-														<input readonly value="<?php echo $k['nip'] ?>" type="number" name="nip" class="form-control">
-													</div>
-												</div>
-												<div class="modal-footer no-bd">
-													<button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-undo"></i> Close</button>
-												</div>
-												</form>
-											</div>
-										</div>
-									</div>
+while ($k = mysqli_fetch_array($q)) {
+?>
 
-								<?php } ?>
+	<div class="modal fade" id="modalDetailBarang<?php echo $k['kodeTransaksi'] ?>" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header no-bd">
+					<h5 class="modal-title">
+						<span class="fw-mediumbold">
+							Detail</span>
+						<span class="fw-light">
+							Barang
+						</span>
+					</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<form method="POST" enctype="multipart/form-data" action="">
+					<div class="modal-body">
+						<input type="hidden" name="id" value="<?php echo $k['kodeTransaksi'] ?>">
+						<div class="form-group">
+							<label>Kode Transaksi</label>
+							<input readonly value="<?php echo $k['kodeTransaksi'] ?>" type="number" name="kodeTransaksi" class="form-control">
+						</div>
+						<div class="form-group">
+							<label>Tanggal</label>
+							<input readonly value="<?php echo $k['tanggal'] ?>" type="date" name="tanggal" class="form-control">
+						</div>
+						<div class="form-group">
+							<label>Jumlah Item</label>
+							<input readonly value="<?php echo $k['jumlahitem'] ?>" type="number" name="jumlahitem" class="form-control">
+						</div>
+						<div class="form-group">
+							<label>Total Pembayaran</label>
+							<input readonly value="<?php echo $k['totalPembayaran'] ?>" type="number" name="totalpembayaran" class="form-control">
+						</div>
+						<div class="form-group">
+							<label>Pegawai yang menangani</label>
+							<input readonly value="<?php echo $k['nip'] ?>" type="number" name="nip" class="form-control">
+						</div>
+					</div>
+					<div class="modal-footer no-bd">
+						<button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-undo"></i> Close</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 
-								<?php
+<?php } ?>
+
+<?php
 
 if (isset($_POST['tampil'])) {
-    echo "Mulai Tanggal: " . $_POST['mulai_tanggal'] . "<br>";
-    echo "Sampai Tanggal: " . $_POST['sampai_tanggal'] . "<br>";
+	echo "Mulai Tanggal: " . $_POST['mulai_tanggal'] . "<br>";
+	echo "Sampai Tanggal: " . $_POST['sampai_tanggal'] . "<br>";
 
 	$mulai_tanggal = $_POST['mulai_tanggal'];
-    $sampai_tanggal = $_POST['sampai_tanggal'];
-    // Query untuk menghitung total omset dalam rentang tanggal
-    $query_omset = "SELECT SUM(totalPembayaran) AS totalOmset
+	$sampai_tanggal = $_POST['sampai_tanggal'];
+	// Query untuk menghitung total omset dalam rentang tanggal
+	$query_omset = "SELECT SUM(totalPembayaran) AS totalOmset
                     FROM transaksi
                     WHERE tanggal BETWEEN '$mulai_tanggal' AND '$sampai_tanggal'";
 
-    $result_omset = mysqli_query($conn, $query_omset);
+	$result_omset = mysqli_query($conn, $query_omset);
 
-    if ($result_omset) {
-        $omset_data = mysqli_fetch_assoc($result_omset);
-        $omzet = $omset_data['totalOmset'];
-        $query_update_omset = "UPDATE omset SET nilai_omset = $omzet, tanggalMulai = '$mulai_tanggal', tanggalSelesai = '$sampai_tanggal' WHERE id = 1";
+	if ($result_omset) {
+		$omset_data = mysqli_fetch_assoc($result_omset);
+		$omzet = $omset_data['totalOmset'];
+		$query_update_omset = "UPDATE omset SET nilai_omset = $omzet, tanggalMulai = '$mulai_tanggal', tanggalSelesai = '$sampai_tanggal' WHERE id = 1";
 
-        mysqli_query($conn, $query_update_omset);
+		mysqli_query($conn, $query_update_omset);
 		echo "<meta http-equiv='refresh' content=0; URL=?view=datalaporan>";
-    } else {
-        echo "Error: " . mysqli_error($conn);
+	} else {
+		echo "Error: " . mysqli_error($conn);
 		echo "<script>alert('Data Tidak Disimpan 3')</script>";
-    }
+	}
 }
 ?>
 
 
 <!-- <?php
 
-// if (isset($_POST['tampil'])) {
-//     // Ambil nilai tanggal mulai dan tanggal sampai dari form
-//     $mulai_tanggal = $_POST['mulai_tanggal'];
-//     $sampai_tanggal = $_POST['sampai_tanggal'];
+		// if (isset($_POST['tampil'])) {
+		//     // Ambil nilai tanggal mulai dan tanggal sampai dari form
+		//     $mulai_tanggal = $_POST['mulai_tanggal'];
+		//     $sampai_tanggal = $_POST['sampai_tanggal'];
 
-//     // Query untuk menghitung total omset dalam rentang tanggal
-//     $query_omset = "SELECT SUM(totalPembayaran) AS totalOmset
-//                     FROM transaksi
-//                     WHERE tanggal BETWEEN '$mulai_tanggal' AND '$sampai_tanggal'";
+		//     // Query untuk menghitung total omset dalam rentang tanggal
+		//     $query_omset = "SELECT SUM(totalPembayaran) AS totalOmset
+		//                     FROM transaksi
+		//                     WHERE tanggal BETWEEN '$mulai_tanggal' AND '$sampai_tanggal'";
 
-//     $result_omset = mysqli_query($conn, $query_omset);
+		//     $result_omset = mysqli_query($conn, $query_omset);
 
-//     if ($result_omset) {
-//         $omset_data = mysqli_fetch_assoc($result_omset);
-//         $total_omset = $omset_data['totalOmset'];
-// 		echo "Total Omset dari $mulai_tanggal sampai $sampai_tanggal adalah: $total_omset";
+		//     if ($result_omset) {
+		//         $omset_data = mysqli_fetch_assoc($result_omset);
+		//         $total_omset = $omset_data['totalOmset'];
+		// 		echo "Total Omset dari $mulai_tanggal sampai $sampai_tanggal adalah: $total_omset";
 
-//         // Tampilkan total omset pada input field "Omzet"
-//         echo '<script>
-//                 document.getElementsByName("omzet")[0].value = "' . number_format($total_omset, 0, ',', '.') . '";
-//               </script>';
-//     } else {
-//         echo "Error: " . mysqli_error($conn);
-//     }
-// }
-?> -->
-
-
-								
+		//         // Tampilkan total omset pada input field "Omzet"
+		//         echo '<script>
+		//                 document.getElementsByName("omzet")[0].value = "' . number_format($total_omset, 0, ',', '.') . '";
+		//               </script>';
+		//     } else {
+		//         echo "Error: " . mysqli_error($conn);
+		//     }
+		// }
+		?> -->
